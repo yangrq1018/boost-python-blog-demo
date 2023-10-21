0d76ab6bedf635ee449b779c11daf107872215d8
xiaoniu <yang.ruoqi@outlook.com>
2023-10-21 23:36:24 +0800

    update

--- inc.cpp
+++ inc.cpp
@@ -5,24 +5,33 @@ using namespace boost::python;
 class IncBase {
 public:
     virtual int inc(int n) = 0;
+};
 
-    int callInc(int n) {
-        return inc(n);
+class IncImp : public IncBase {
+public:
+    int inc(int n) override {
+        return n + 1;
     }
 };
 
-class IncBaseWrap: public IncBase, public wrapper<IncBase> {
+int callInc(IncBase& i, int n) {
+    return i.inc(n);
+}
+
+class IncBaseWrap : public IncBase, public wrapper<IncBase> {
 public:
-    int inc(int n) {
+    int inc(int n) override {
         // calls python override
         return this->get_override("inc")(n);
     }
 };
 
-
 BOOST_PYTHON_MODULE(inc) {
+    // clang-format off
     class_<IncBaseWrap, boost::noncopyable>("IncBase")
-    .def(init<>())
-    .def("callInc", &IncBase::callInc)
-    ;
+        .def(init<>());
+    class_<IncImp, bases<IncBase>, boost::noncopyable>("IncImp")
+        .def(init<>());
+    def("callInc", callInc, (arg("imp"), arg("n")));
+    // clang-format on
 }
--- test_ext_module.py
+++ test_ext_module.py
@@ -1,6 +1,7 @@
 from mymath import add
 from student import Student
-from inc import IncBase
+from inc import IncBase, callInc
+
 
 def test_add():
     assert add(2, 3) == 5
@@ -13,19 +14,22 @@ def test_student_make():
     s = Student()
     assert s.name == "foo"
 
+
 def test_py_override():
     class MyInc(IncBase):
         def inc(self, n):
             return n + 1
     f = MyInc()
-    assert f.callInc(3) == 4
-    assert f.callInc(-1) == 0
+    assert callInc(f, 3) == 4
+    assert callInc(f, -1) == 0
+
 
 def test_student_iter():
     s = Student()
     assert list(s) == ["bar", "baz"]
     s.makeFriend(Student("qux", 80))
     assert list(s) == ["bar", "baz", "qux"]
+
 
 def test_student_add():
     s1 = Student("foo", 20)
